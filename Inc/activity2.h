e1b93c51b0928c4db9f5718a61bce1dfdc5bef45
259141AnushaD <54492524+259141AnushaD@users.noreply.github.com>
2021-04-30 22:57:50 +0530

    Update and rename activity_2.h to activity2.h

---
+++ Inc/activity2.h
@@ -0,0 +1,14 @@
+#ifndef _ACTIVITY2_
+#define _ACTIVITY2_
+
+#ifndef __AVR_ATmega328__
+    #define __AVR_ATmega328__
+#endif
+
+#include <avr/io.h>
+
+void InitAdc();
+uint16_t ReadAdc(uint8_t ch);
+void activity_2();
+
+#endif
