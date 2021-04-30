266008d22ffd2cb0506bd92d2923726f1793d6f5
259141AnushaD <54492524+259141AnushaD@users.noreply.github.com>
2021-04-30 22:54:32 +0530

    Update and rename activity_1.h to activity1.h

---
+++ Inc/activity1.h
@@ -0,0 +1,20 @@
+#ifndef _ACTIVITY1_
+#define _ACTIVITY1_
+
+#ifndef __AVR_ATmega328__
+    #define __AVR_ATmega328__
+#endif
+
+#define LED_PORT (PORTB) // led port number
+#define LED_PIN (PB0) // led pin number
+#define DRIVER_PIN (PC6) // to check whether the driver is seated
+#define HEATER_PIN (PD4) // to check whether the heater is turned on
+
+#include <avr/io.h>
+
+void pin_init();
+void activity1_loop();
+void activity_1();
+
+#endif
+© 2021 GitHub, Inc.
